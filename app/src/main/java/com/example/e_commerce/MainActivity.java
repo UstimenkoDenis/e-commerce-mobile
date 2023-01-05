61a39c291135c911c550a1eca9b95e86625db661
Ustim <ustimenkodenis@mail.ru>
2023-01-05 16:52:20 +0300

    feat: add categories to course model

--- app/src/main/java/com/example/e_commerce/MainActivity.java
+++ app/src/main/java/com/example/e_commerce/MainActivity.java
@@ -18,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView categoryRecycler, courseRecycler;
     CategoryAdapter categoryAdapter;
-    CourseAdapter courseAdapter;
+    static CourseAdapter courseAdapter;
+    static List<Course> courseList = new ArrayList<>();
+    static List<Course> fullCoursesList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +35,16 @@ public class MainActivity extends AppCompatActivity {
 
         setCategoryRecycler(categoryList);
 
-        List<Course> courseList = new ArrayList<>();
         courseList.add(new Course(1, "java", "Профессия Java\nразработчик", "1 января", "начальный", "#424345", "За программу вы изучите построение графических приложений под ПК,\n" +
                 "разработку веб сайтов на основе Java Spring Boot, изучите построение полноценных Андроид приложений\n" +
-                "и отлично изучите сам язык Джава!"));
-        courseList.add(new Course(2, "python", "Профессия Python\nразработчик", "10 января", "начальный", "#9FA52D", "test"));
+                "и отлично изучите сам язык Джава!", 3));
+        courseList.add(new Course(2, "python", "Профессия Python\nразработчик", "10 января", "начальный", "#9FA52D", "test", 3));
+        courseList.add(new Course(3, "unity", "Профессия Unity\nразработчик", "6 января", "начальный", "#690058", "test", 1));
+        courseList.add(new Course(4, "front_end", "Профессия Front-end\nразработчик", "9 января", "начальный", "#F39887", "test", 2));
+        courseList.add(new Course(5, "node_js", "Профессия Node-js\nразработчик", "7 января", "начальный", "#A2CAE3", "test", 2));
+        courseList.add(new Course(6, "full_stack", "Профессия Full-stack\nразработчик", "2 февраля", "начальный", "#242967", "test", 2));
+
+        fullCoursesList.addAll(courseList);
 
         setCourseRecycler(courseList);
     }
@@ -60,5 +67,22 @@ public class MainActivity extends AppCompatActivity {
 
         categoryAdapter = new CategoryAdapter(this, categoryList);
         categoryRecycler.setAdapter(categoryAdapter);
+    }
+
+    public static void showCoursesByCategory(int category) {
+        courseList.clear();
+        courseList.addAll(fullCoursesList);
+
+        List<Course> filterCourses = new ArrayList<>();
+
+        for(Course c: courseList) {
+            if(c.getCategory() == category)
+                filterCourses.add(c);
+        }
+
+        courseList.clear();
+        courseList.addAll(filterCourses);
+
+        courseAdapter.notifyDataSetChanged();
     }
 }
